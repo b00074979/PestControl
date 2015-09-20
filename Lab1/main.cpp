9214129b74642f10647860913cf1527fd6b8b963
Aleena Chaudhry <B00074979>
2015-09-20 20:00:45 +0100

    Assignment part 1

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -15,6 +15,8 @@ public:
     {
         height = h;
     }
+
+
 protected:
     int width;
     int height;
@@ -26,7 +28,18 @@ class Triangle: public Shape
 public:
     int getArea()
     {
+
         return (width*height)/2;
+    }
+};
+
+class Rectangle: public Shape
+{
+
+public:
+    int getArea()
+    {
+        return width*height;
     }
 };
 
@@ -37,6 +50,11 @@ int main()
     Tri.setHeight(7);
 
     cout << "Total Triangle area: " << Tri.getArea() << endl;
+
+    Rectangle Rec;
+    Rec.setWidth(2);
+    Rec.setHeight(4);
+    cout << "Area of Rectangle: " << Rec.getArea() << endl;
 
     return 0;
 }
